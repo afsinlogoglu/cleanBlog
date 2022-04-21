3ee1b4ab17af47056e7d8b9adb695af5d975ede1
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-22 00:21:51 +0300

    routing added

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require("express");
+const mongoose = require('mongoose'); 
+
+const ejs = require("ejs");
+const Post = require('./models/Post');
 
 const app = express();
-const ejs = require("ejs");
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+
+//db connection
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//template engine
+app.set('view engine', 'ejs');
+
+
+//middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port:${port}`);
 });
+
+
 //routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', async(req, res) => {
+  const posts = await Post.find({})
+  res.render('index',{
+    posts
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -23,4 +46,9 @@ app.get("/post", (req, res) => {
 
 app.get("/addPost", (req, res) => {
   res.render("add_post");
+});
+
+app.post('/posts', async(req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
