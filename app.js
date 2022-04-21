0555de654e5c0219635f0c014c12104a16e3c473
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-21 03:13:07 +0300

    design updated

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port:${port}`);
+});
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-const port = 3000
-app.listen(port,()=>{
-    console.log(`Server started on port:${port}`)
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.get('/',(req,res)=>{
-    const blog ={
-        id:1,
-        title:"Blog Title",
-        description:"Blog Desc"
-    }
-    res.send(blog);
-})
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/addPost", (req, res) => {
+  res.render("add_post");
+});
